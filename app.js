b0ab2b5887fb0c5b6cc5e952af8e26bcce278f8d
Cooper Pearson <mptprogesterone@gmail.com>
2020-06-29 15:25:49 -0500

    script

---
+++ app.js
@@ -0,0 +1,23 @@
+// GAME FUNCTION:
+// -Player must guess a number between min and max
+// -Player gets a certain amount of guesses
+// -Notify player of guesses remaining
+// -Let player choose to play again
+
+// Game values
+let min = 1,
+    max = 10,
+    winningNum = 2,
+    guessesLeft = 3;
+
+//UI Elements
+const game = document.querySelector('#game'),
+    minNum = document.querySelector('.min-num'),
+    maxNum = document.querySelector('.max-num'),
+    guessBtn = document.querySelector('#guess-btn'),
+    guessInput = document.querySelector('#guess-input'),
+    message = document.querySelector('.message');
+
+//Assign UI min and max
+minNum.textContent = min;
+maxNum.textContent = max;
